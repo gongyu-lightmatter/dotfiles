059e703539fbcac837f8b0207dac9c7e629367e8
Gongyu Wang <gongyu@lightmatter.co>
2022-04-11 00:09:28 -0400

    Update setup_shell.sh

--- setup_shell.sh
+++ setup_shell.sh
@@ -9,23 +9,31 @@ sudo apt install zsh
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install autojump
-sudo apt install autojump
-
 # install powerlevel 10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# make sym links
+ln -s $(pwd)/.zshrc ~/
+
 # install delta, bat
 sudo apt install delta bat
+# install autojump
+sudo apt install autojump
+# alternatively install zoxide 
+curl -sS https://webinstall.dev/zoxide | bash
 
 # install lazygit
 sudo add-apt-repository ppa:lazygit-team/release
 sudo apt-get update
 sudo apt-get install lazygit
 
-# make sym links
-ln -s ./.zshrc ~/
+# install fzf
+sudo apt install fzf
 
+# make more sym links
+ln -s $(pwd)/.gitconfig ~
+ln -s $(pwd)/config.yml ~/.config/lazygit/
+ln -s $(pwd)/.tmux.conf ~
 
 
 # remind to restart shell
